818a6abc6614caadaefc8248decf31d0f20615ae
Justine Joshua Tan <justinejoshuatan@yahoo.com>
2026-02-20 19:00:11 +0800

    added about section
    1. Added description of myself
    2. Added list of proficiencies

--- tan-portfolio/src/lib/About.svelte
+++ tan-portfolio/src/lib/About.svelte
@@ -2,17 +2,41 @@
     // Script for about me
 </script>
     
-<main class="min-h-full flex flex-col items-center justify-center bg-gray-50 dark:bg-black">
+<main class="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-black">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" id="about">
         <div class="flex flex-col lg:flex-row items-center">
             <div class="justify-center md:text-left mr-10">
                 <h1 class="text-5xl font-bold text-red-600 mb-4 google-sans-code-major">
                     // about me
                 </h1>
-
+                
+                <h3 class="text-3xl font-bold text-gray-700 google-sans-code-major">
+                    # description
+                </h3>
+                
                 <p class="mt-8 text-gray-600 text-wrap text-justify max-w-2xl mx-auto px-4">
-                    
+                    I am a Computer Science student at De La Salle University, Manila, majoring in Software Technology. I am looking for an internship 
+                    where I can improve my skills in regards to not only programming, but also teamwork, and gain experience in a software development setting.
+                    I do not have an exact specification, but I am open to any software development internship, but I am most interested in web development,
+                    quality assurance, cloud computing, and database management (in that order).
+                    <br><br>
+
+                    Outside of school, I primarily enjoy playing video games, such as the Yakuza series. I also enjoy reading manga, and listening to music.
+                    <br><br>
                 </p>
+
+                <h3 class="text-3xl font-bold text-gray-700 google-sans-code-major">
+                    # proficiencies
+                </h3>
+                <ul class="list-disc mt-8 text-gray-600 text-wrap text-justify max-w-2xl mx-auto px-4">
+                    <li>C++, Java, Python, JavaScript, HTML and CSS</li>
+                    <li>Selenium IDE, and limited knowledge of Jest</li>
+                    <li>Database management (MySQL, MongoDB, Rapidminer)</li>
+                    <li>Relearning HTML, CSS, and JavaScript via SvelteKit, learning Tailwind</li>
+                    <li>Working towards being an AWS Certified Cloud Practitioner</li>
+                </ul>
+
+                
             </div>
         </div>
     </div>
