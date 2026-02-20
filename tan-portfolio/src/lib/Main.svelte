e71a35327b69ad4b1e3b302ad363027e5b7a555b
Justine Joshua Tan <justinejoshuatan@yahoo.com>
2026-02-20 18:59:02 +0800

    more syncing, modified main for the image
    1. changed description
    2. changed positioning of image (justify-center wfull mt-8 md:mt-0)
    3. added shrink-0 w-full to my photo

--- tan-portfolio/src/lib/Main.svelte
+++ tan-portfolio/src/lib/Main.svelte
@@ -6,7 +6,7 @@
 <main class="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-black">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div class="flex flex-col lg:flex-row items-start" id="main">
-            <div class="justify-center md:text-left mr-10">
+            <div class="justify-center md:text-left md:w-1/2">
                 <img src={logoImg} alt="Justine Tan Logo" class="w-32 h-32">
                 <h1 class="text-5xl font-bold text-red-600 mb-4 google-sans-code-major">
                     // welcome
@@ -17,12 +17,14 @@
                 </h2>
 
                 <p class="mt-8 text-gray-600 text-wrap text-justify max-w-2xl mx-auto px-4">
-                    I am a Computer Science student at De La Salle University, Manila, majoring in Software Technology.
-                    I am looking for an internship where I can improve my skills in regards to not only programming, but also teamwork, and gain experience in a software development setting.
+                    Computer Science student <br />
+                    De La Salle University, Manila
                 </p>
+
+                <!-- icons -->
             </div>
-            <div id="main_image" class="md:w-1/2 flex justify-around">
-                <img src={profileImg} alt="Justine Tan" id="profile_img" class="profile_img absolute">
+            <div id="main_image" class="md:w-1/2 flex justify-center w-full mt-8 md:mt-0">
+                <img src={profileImg} alt="Justine Tan" id="profile_img" class="profile_img shrink-0 w-full">
             </div>
         </div>
     </div>
